16a0e77ecb7dbb8da93ee2a534f7f620df4f5478
pranavkr175 <pranavkr175@gmail.com>
2023-10-23 15:45:31 +0530

    Initial commit

---
+++ src/main/java/Palindrom/PalindromeChecker.java
@@ -0,0 +1,38 @@
+package Palindrom;
+
+	import java.util.Scanner;
+
+	public class PalindromeChecker {
+
+	    public static void main(String[] args) {
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.print("Enter a string: ");
+	        String input = scanner.nextLine();
+
+	        if (isPalindrome(input)) {
+	            System.out.println("The string "+input+" is a palindrome.");
+	        } else {
+	            System.out.println("The string "+input+"is not a palindrome.");
+	        }
+
+	        scanner.close();
+	    }
+
+	    public static boolean isPalindrome(String str) {
+	      
+	        str = str.replaceAll("\\s", "").toLowerCase();
+	        
+	        int length = str.length();
+	        
+	        for (int i = 0; i < length / 2; i++) {
+	            if (str.charAt(i) != str.charAt(length - i - 1)) {
+	                return false;
+	            }
+	        }
+	        
+	        return true; 
+	    }
+	}
+
+
